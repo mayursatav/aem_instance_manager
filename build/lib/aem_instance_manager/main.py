3a41f67c9d0a7b2fa1ebb7dd807229f1cdee0b70
mayursatav <mayursatav9@gmail.com>
2025-04-13 17:40:49 +0530

    v0.4 added debug and view src folde roption

--- build/lib/aem_instance_manager/main.py
+++ build/lib/aem_instance_manager/main.py
@@ -5,11 +5,13 @@ import subprocess
 import os
 import socket
 import time
+import sys
+import re
 
 class ProjectManagerApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("AEM Instance Manager By Mayur Satav")
+        self.root.title("AEM Instance Manager")
 
         # Variables
         self.projects = pd.DataFrame(columns=["Project Name", "Author Port", "Publish Port", "Folder Path", "Status"])
@@ -32,25 +34,35 @@ class ProjectManagerApp:
         self.publish_port_entry = ttk.Entry(self.input_frame, textvariable=self.current_publish_port,validate="key", validatecommand=(root.register(self.validate_publish_port), '%P'), width=10)
         self.folder_path_label = ttk.Label(self.input_frame, text="Folder Path:")
         self.folder_path_entry = ttk.Entry(self.input_frame, textvariable=self.current_folder_path, width=10)
-        self.save_button = ttk.Button(self.input_frame, text="Save", command=self.save_project)
-        self.exit_button = ttk.Button(self.input_frame, text="Exit", command=root.destroy)
+        self.save_button = ttk.Button(self.input_frame, text="Save", command=self.save_project, width=6)
+        self.exit_button = ttk.Button(self.input_frame, text="Exit", command=root.destroy, width=6)
         self.project_display_error = tk.Text(self.input_frame, height=3.5, width=10, state=tk.DISABLED, bg="black", fg="#00FF00")
 
         # Right side: Project controls
-        self.project_controls_frame = ttk.Frame(root)
+        self.project_controls_frame = ttk.Frame(root, borderwidth=0.5, relief="solid")
         self.project_controls_frame.grid(row=0, column=1, padx=10, pady=5, sticky="nsew")
 
         self.project_dropdown_label = ttk.Label(self.project_controls_frame, text="Select Project:")
-        self.project_dropdown = ttk.Combobox(self.project_controls_frame, values=[], textvariable=self.selected_project)
+        self.project_dropdown = ttk.Combobox(self.project_controls_frame, values=[], textvariable=self.selected_project, state="readonly")
 
         # self.extra_args_label = ttk.Label(self.project_controls_frame, text="Extra Args:")
         # self.extra_args_entry = ttk.Entry(self.project_controls_frame, width=20)
+
+        # Debug Mode Checkbox
+        self.debug_mode = tk.BooleanVar(value=False)
+        self.debug_checkbox = ttk.Checkbutton(self.project_controls_frame, text="Debug Mode", variable=self.debug_mode)
+        self.debug_checkbox.grid(row=1, column=0, padx=10, pady=5, sticky="w")
+
+        # Open Project Folder Button
+        self.open_folder_button = ttk.Button(self.project_controls_frame, text="Open Project Folder", command=self.open_project_folder)
+        self.open_folder_button.grid(row=1, column=1, padx=10, pady=1, sticky="w")
+
 
         self.start_button = ttk.Button(self.project_controls_frame, text="Start", command=self.start_project)
         self.stop_button = ttk.Button(self.project_controls_frame, text="Stop", command=self.stop_project)
         self.remove_button = ttk.Button(self.project_controls_frame, text="Remove", command=self.remove_project)
         # self.project_display_label = ttk.Label(self.project_controls_frame, text="Project Details:")
-        self.project_display_text = tk.Text(self.project_controls_frame, height=14, width=30, state=tk.DISABLED)
+        self.project_display_text = tk.Text(self.project_controls_frame, height=12, width=30, state=tk.DISABLED)
         
 
         # Grid layout
@@ -90,12 +102,33 @@ class ProjectManagerApp:
 
         # Binding event for dropdown selection
         self.project_dropdown.bind("<<ComboboxSelected>>", self.display_project_details)
+
+        # Footer Label
+        self.footer_label = ttk.Label(root, text="Created by Mayur Satav                   www.mayursatav.in                   Version v0.4", anchor="center", font=("Helvetica", 9), foreground="gray")
+        self.footer_label.grid(row=1, column=0, columnspan=2, pady=5, sticky="ew")
     
     def display_messages(self, message):
         self.project_display_error.config(state=tk.NORMAL)
         self.project_display_error.delete("1.0", tk.END)
         self.project_display_error.insert(tk.END, f"{message}\n")
         self.project_display_error.config(state=tk.DISABLED)
+
+    def open_project_folder(self):
+        selected_project_name = self.selected_project.get()
+        if selected_project_name:
+            project_index = self.projects.index[self.projects["Project Name"] == selected_project_name][0]
+            folder_path = self.projects.at[project_index, "Folder Path"]
+
+            # Open the folder using the default file explorer
+            try:
+                if os.name == 'nt':  # For Windows
+                    os.startfile(folder_path)
+                elif os.name == 'posix':  # For MacOS and Linux
+                    subprocess.run(["open", folder_path] if sys.platform == "darwin" else ["xdg-open", folder_path])
+                else:
+                    self.display_messages(f"Unsupported OS for opening folder: {os.name}")
+            except Exception as e:
+                self.display_messages(f"Error opening folder: {str(e)}")
 
     def validate_project_name(self, new_value):
         return len(new_value) <= 10
@@ -141,6 +174,11 @@ class ProjectManagerApp:
         author_port = self.current_author_port.get()
         publish_port = self.current_publish_port.get()
         folder_path = self.current_folder_path.get()
+
+        if not re.search(r"/$", folder_path):
+            folder_path += "/"
+
+        print(folder_path)
         
         if project_name and author_port:
             new_project = pd.DataFrame({
@@ -176,28 +214,36 @@ class ProjectManagerApp:
 
     def start_project(self):
         selected_project_name = self.selected_project.get()
-        
+
         if selected_project_name:
             project_index = self.projects.index[self.projects["Project Name"] == selected_project_name][0]
             author_port = self.projects.at[project_index, "Author Port"]
             publish_port = self.projects.at[project_index, "Publish Port"]
             folder_path = self.projects.at[project_index, "Folder Path"]
 
-            authorPath= os.path.join(folder_path,"author")
-            publishPath= os.path.join(folder_path,"publish")
+            authorPath = os.path.join(folder_path, "author")
+            publishPath = os.path.join(folder_path, "publish")
             authorJar = self.print_jar_files_in_folder(authorPath)
             publishJar = self.print_jar_files_in_folder(publishPath)
-            print (authorJar)
-            print (publishJar)
+            print(authorJar)
+            print(publishJar)
+
             # Check if the author and publish ports are already in use
             if self.is_port_in_use(author_port) or self.is_port_in_use(publish_port):
                 self.display_messages(f"another project already using both {author_port} and {publish_port} ports")
             else:
-                
-                author_command = f"cd {authorPath} ;java -jar {authorJar} -p {author_port}"
-                publish_command = f"cd {publishPath} ;java -jar {publishJar} -p {publish_port}"
-                print (author_command)
-                print (publish_command)
+                if self.debug_mode.get():  # If Debug Mode is checked
+                    # Debug mode command
+                    author_command = f"cd {authorPath} ; java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar {authorJar} -p {author_port}"
+                    publish_command = f"cd {publishPath} ; java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar {publishJar} -p {publish_port}"
+                else:
+                    # Regular command
+                    author_command = f"cd {authorPath} ; java -jar {authorJar} -p {author_port}"
+                    publish_command = f"cd {publishPath} ; java -jar {publishJar} -p {publish_port}"
+
+                print(author_command)
+                print(publish_command)
+
                 # Run the command in the background
                 self.run_command_in_background(author_command)
                 self.run_command_in_background(publish_command)
